6c20a6e77a54e29717ec1e432d65844fae123a7b
sowabdulai <abdulai-as@hotmail.com>
2022-10-04 20:55:03 -0400

    magic ball

--- node/first-express-lab/index.js
+++ node/first-express-lab/index.js
@@ -26,13 +26,15 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
 const random = [
   "It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"
 ]
-const milRandom = Math.floor(Math.random()) * random
+const milRandom = random[Math.floor(Math.random() * random.length)] 
 console.log(milRandom);
-app.get('/magic/:', (req, res) => {
-  
+app.get('/magic/:id', (req, res) => {
+  const { id } = req.params
+  const url = 'Will%20I%20Be%20A%20Millionaire'
+  res.send(`<h1>${url} <br/> <h1/> <h1>${milRandom}<h1/>`)
+  // res.send(id === url ? `<h1>${milRandom}<h1/>` && `<h1> ${url} <h1/>` : '')
+ 
 })
-
-
 
 
 
