95f7ef47c4cab823c84f922f31900131f26b1741
sowabdulai <abdulai-as@hotmail.com>
2022-10-05 21:22:23 -0400

    my server is set up and running

---
+++ node/pokeExpress-hw/server.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+const port = 5000
+
+app.get('/', (req, res) => {
+  res.send('welcome to the pokemon App!')
+})
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}...`);
+})
