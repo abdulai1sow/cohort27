093faed0184211deaecd58930703e7a068039d76
sowabdulai <abdulai-as@hotmail.com>
2022-10-05 21:58:35 -0400

    connected my database, can see json in the browser

--- node/pokeExpress-hw/server.js
+++ node/pokeExpress-hw/server.js
@@ -1,10 +1,16 @@
-import express from 'express'
+const express = require('express')
+const pokemons = require('./models/pokemon')
 
 const app = express()
 const port = 5000
 
 app.get('/', (req, res) => {
   res.send('welcome to the pokemon App!')
+})
+//show all pokemon
+app.get('/pokemons', (req, res) => {
+  console.log(pokemons);
+  res.send(pokemons)
 })
 
 app.listen(port, () => {
